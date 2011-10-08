585e8337b66446bfed51d1c3ee4c0ddddf632a3c
James Dean Shepherd <jamesds2007@gmail.com>
2011-10-08 16:08:01 +0100

    Removed cheap install script, converted README to Markdown

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Putting a symbolic link into /usr/bin. Run with 'pass_crypt'"
-echo "\nProtip: put the following alias into your .bash_aliases file"
-echo "alias pc='pass_crypt'"
-
-ln -s $PWD/pass_crypt.rb /usr/bin/pass_crypt
